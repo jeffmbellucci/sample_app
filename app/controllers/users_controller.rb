1630e6795579941b11b5411114de4def2b31e37e
Jeff Bellucci <jeffmbellucci@gmail.com>
2013-08-11 20:03:31 -0700

    added user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
   def new
-    render :new
+    @user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the jungle!!"
+      render :show
+    else
+      render :new
+    end
   end
 end
